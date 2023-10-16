40a8c2d0a5dca5f47ec7c750634e04b120b1c84c
Aswin-04 <e.aswin004@gmail.com>
2023-10-16 10:08:24 +0530

    Works fine

--- script.js
+++ script.js
@@ -5,80 +5,86 @@ const DEFAULT_MODE = "blackSketch";
 
 const container = document.body.querySelector(".container");
 const slider = document.body.querySelector(".slider");
-const span = slider.nextElementSibling;
+const gridSizeLabel = slider.nextElementSibling;
+const clearBtn = document.querySelector(".clear-btn");
+const blackSketchBtn = document.querySelector(".black-sketch-btn");
+const eraseBtn = document.querySelector(".erase-btn");
+const rainbowBtn = document.querySelector(".rainbow-btn"); 
 
 let isDrawing = false;
-let color = DEFAULT_COLOR;
-let currentMode = DEFAULT_MODE
-let currentSize = slider.value
+let currentColor = DEFAULT_COLOR;
+let currentMode = DEFAULT_MODE;
+let currentGridSize = DEFAULT_SIZE;
+
+createGrid(currentGridSize); 
 
 slider.addEventListener("input", () => {
-    const gridSize = slider.value;
-    span.textContent = `${gridSize} x ${gridSize}`;
-    createGrid(gridSize);
+    currentGridSize = slider.value;
+    gridSizeLabel.textContent = `${currentGridSize} x ${currentGridSize}`;
+    createGrid(currentGridSize);
 });
 
 document.body.addEventListener("mousedown", () => isDrawing = true);
 document.body.addEventListener("mouseup", () => isDrawing = false);
 
+createEventListener(eraseBtn, "eraser");
+createEventListener(blackSketchBtn, DEFAULT_MODE);
+createEventListener(rainbowBtn,"rainbow")
+
 function createGrid(gridSize) {
     container.innerHTML = "";
-
+    
     for (let i = 1; i <= gridSize; i++) {
         const row = document.createElement("div");
         row.style.cssText = `max-height : ${640/gridSize}px;
-                             display : flex;`
+        display : flex;`
         
         for (let j=1; j <= gridSize; j++) {
             const box = document.createElement("div");
             box.style.cssText = `height : ${640/gridSize}px; 
-                                 width : ${640/gridSize}px;
-                                 box-sizing : border-box;
-                                 border : 1px solid black; `;
-
+            width : ${640/gridSize}px;
+            box-sizing : border-box;
+            border : 1px solid black; `;
+            
             box.classList = "eachBox";
             
             box.addEventListener("mouseover", (event) => changeColor(event, color));
             box.addEventListener("mousedown", (event) => changeColor(event, color));
             row.append(box);
         }
-
+        
         container.append(row);
-
+        
     }
 }
 
 function changeColor(event, color) {
     if (isDrawing && event.buttons === 1) {
-
+        
         if (currentMode === DEFAULT_MODE) {
             color = DEFAULT_COLOR;
-
+            
         } else if (currentMode === "eraser") {
             color = "#edf1f4";
-
+            
         } else if (currentMode === "rainbow") {
-
+            
             const randomR = Math.floor(Math.random() * 256);
             const randomG = Math.floor(Math.random() * 256);
             const randomB = Math.floor(Math.random() * 256);
-
+            
             color = `rgb(${randomR}, ${randomG}, ${randomB})`;
         }
         event.target.style.backgroundColor = color;
     } 
 }
 
-
-const clearBtn = document.querySelector(".clear-btn");
-const blackSketchBtn = document.querySelector(".black-sketch-btn");
-const eraseBtn = document.querySelector(".erase-btn");
-const rainbowBtn = document.querySelector(".rainbow-btn"); 
-
 function createEventListener(element, mode) {
     if (mode === "clear") {
-        element.addEventListener("click", () => createGrid(currentSize))
-      
+        element.addEventListener("click", () => {
+            createGrid(currentGridSize);
+        });
+        
     } else {
         element.addEventListener("click", () => {
             setCurrentMode(mode)           
@@ -86,13 +92,9 @@ function createEventListener(element, mode) {
     }
 }
 
-
-
-function setCurrentMode(newMode) {
-    
+function setCurrentMode(newMode) {   
     activateButton(newMode);
-    currentMode = newMode;
-    
+    currentMode = newMode;    
 }
 
 function activateButton(newMode) {
@@ -104,28 +106,17 @@ function activateButton(newMode) {
     } else {
         blackSketchBtn.classList.remove("active")
     }
-
+    
     if (newMode === "rainbow") {
         rainbowBtn.classList.add("active")
     } else if (newMode === "eraser") {
         eraseBtn.classList.add("active")
     } else {
         blackSketchBtn.classList.add("active")
-    }
-    
-    
-    
+    }   
 }
 
 
 
 
-
-
-createGrid(DEFAULT_SIZE); 
-
-
-createEventListener(eraseBtn, "eraser");
-createEventListener(blackSketchBtn, DEFAULT_MODE);
-createEventListener(rainbowBtn,"rainbow")
 createEventListener(clearBtn, "clear")
