547c9b3bb47d95e274cf391663d246e79aa38ba9
Aswin-04 <e.aswin004@gmail.com>
2023-10-16 09:43:29 +0530

    Add EventListener to rainbowBtn

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+const DEFAULT_SIZE = 16;
+const DEFAULT_COLOR = "black";
+const DEFAULT_MODE = "blackSketch";
+
+
 const container = document.body.querySelector(".container");
 const slider = document.body.querySelector(".slider");
 const span = slider.nextElementSibling;
 
 let isDrawing = false;
-let defaultColor = "black";
-let color = defaultColor;
+let color = DEFAULT_COLOR;
+let currentMode = DEFAULT_MODE
+let currentSize = slider.value
 
 slider.addEventListener("input", () => {
     const gridSize = slider.value;
@@ -44,22 +50,82 @@ function createGrid(gridSize) {
 
 function changeColor(event, color) {
     if (isDrawing && event.buttons === 1) {
+
+        if (currentMode === DEFAULT_MODE) {
+            color = DEFAULT_COLOR;
+
+        } else if (currentMode === "eraser") {
+            color = "#edf1f4";
+
+        } else if (currentMode === "rainbow") {
+
+            const randomR = Math.floor(Math.random() * 256);
+            const randomG = Math.floor(Math.random() * 256);
+            const randomB = Math.floor(Math.random() * 256);
+
+            color = `rgb(${randomR}, ${randomG}, ${randomB})`;
+        }
         event.target.style.backgroundColor = color;
     } 
 }
 
-createGrid(slider.value); 
 
-const clearButton = document.querySelector(".clear");
-clearButton.addEventListener("click", () => createGrid(slider.value));
+const clearBtn = document.querySelector(".clear-btn");
+const blackSketchBtn = document.querySelector(".black-sketch-btn");
+const eraseBtn = document.querySelector(".erase-btn");
+const rainbowBtn = document.querySelector(".rainbow-btn"); 
 
-const eraseButton = document.querySelector(".eraser");
-eraseButton.addEventListener("click", () => {
-    color = "#edf1f4";
-});
+function createEventListener(element, mode) {
+    if (mode === "clear") {
+        element.addEventListener("click", () => createGrid(currentSize))
+      
+    } else {
+        element.addEventListener("click", () => {
+            setCurrentMode(mode)           
+        })
+    }
+}
 
-const blackSketch = document.querySelector(".black-sketch");
-blackSketch.addEventListener("click", () => {
-    color = defaultColor;
-})
 
+
+function setCurrentMode(newMode) {
+    
+    activateButton(newMode);
+    currentMode = newMode;
+    
+}
+
+function activateButton(newMode) {
+    
+    if (currentMode === "rainbow") {
+        rainbowBtn.classList.remove("active")
+    } else if (currentMode === "eraser") {
+        eraseBtn.classList.remove("active")
+    } else {
+        blackSketchBtn.classList.remove("active")
+    }
+
+    if (newMode === "rainbow") {
+        rainbowBtn.classList.add("active")
+    } else if (newMode === "eraser") {
+        eraseBtn.classList.add("active")
+    } else {
+        blackSketchBtn.classList.add("active")
+    }
+    
+    
+    
+}
+
+
+
+
+
+
+createGrid(DEFAULT_SIZE); 
+
+
+createEventListener(eraseBtn, "eraser");
+createEventListener(blackSketchBtn, DEFAULT_MODE);
+createEventListener(rainbowBtn,"rainbow")
+createEventListener(clearBtn, "clear")
